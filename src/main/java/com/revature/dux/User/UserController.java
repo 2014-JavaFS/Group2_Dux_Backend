a974bd0a977dd1f4ed1106573383ee349b1689f1
Ryan Heron <rwh60@txstate.edu>
2024-07-26 17:04:31 -0500

    updated userController to basic functionality

--- src/main/java/com/revature/dux/User/UserController.java
+++ src/main/java/com/revature/dux/User/UserController.java
@@ -1,7 +1,11 @@
 package com.revature.dux.User;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/users")
@@ -11,5 +15,27 @@ public class UserController {
     @Autowired
     public UserController(UserService userService) { this.userService = userService; }
 
-    //TODO https action mapping
+    @GetMapping
+    private ResponseEntity<List<User>> findAllUsers() { return ResponseEntity.ok().body(userService.findAll()); }
+
+    @GetMapping("/{username}")
+    private ResponseEntity<User> findUserByUsername(@PathVariable String username) {
+        return ResponseEntity.ok().body(userService.findByUsername(username));
+    }
+
+    @PostMapping
+    private ResponseEntity<User> postNewUser(@RequestBody User newUser) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(userService.create(newUser));
+    }
+
+    @PutMapping
+    private ResponseEntity<Boolean> putUpdateDuck(@RequestBody User updateUser) {
+        return ResponseEntity.ok(userService.update(updateUser));
+    }
+
+    @DeleteMapping
+    private ResponseEntity<Void> deleteUser(@RequestBody User user) {
+        userService.delete(user);
+        return ResponseEntity.noContent().build();
+    }
 }
