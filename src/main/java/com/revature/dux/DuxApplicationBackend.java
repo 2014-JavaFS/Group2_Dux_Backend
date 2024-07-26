67ff0ec15f1774b5195a970b3bb0e090bb778772
David Jeske <dcjeske@tamu.edu>
2024-07-26 15:52:57 -0500

    DuxApplicationBackend launches

---
+++ src/main/java/com/revature/dux/DuxApplicationBackend.java
@@ -0,0 +1,9 @@
+package com.revature.dux;
+
+import org.springframework.boot.SpringApplication;
+
+public class DuxApplicationBackend {
+    public static void main(String[] args) {
+        SpringApplication.run(DuxApplicationBackend.class, args);
+    }
+}
