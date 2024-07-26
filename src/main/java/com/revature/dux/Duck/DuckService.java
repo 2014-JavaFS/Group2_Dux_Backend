ae9008fc7509f7e2ea8db4576136fc5331a4cf32
David Jeske <dcjeske@tamu.edu>
2024-07-26 12:06:23 -0500

    basic DuckController methods done

--- src/main/java/com/revature/dux/Duck/DuckService.java
+++ src/main/java/com/revature/dux/Duck/DuckService.java
@@ -1,10 +1,13 @@
 package com.revature.dux.Duck;
 
+import com.revature.dux.util.interfaces.Serviceable;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
-public class DuckService {
+public class DuckService implements Serviceable<Duck> {
     private DuckRepository duckRepository;
 
     @Autowired
@@ -12,5 +15,30 @@ public class DuckService {
         this.duckRepository = duckRepository;
     }
 
+    @Override
+    public List<Duck> findAll() {
+        return List.of();
+    }
 
+    @Override
+    public Duck create(Duck newObject) {
+        return null;
+    }
+
+    @Override
+    public Duck findById(int number) {
+        return null;
+    }
+
+    public Duck findByName(String name) {
+        return null;
+    }
+
+    public Boolean update(Duck updateDuck) {
+        return false;
+    }
+
+    public void delete(Duck duck) {
+
+    }
 }
