d568d872f5bf12bfcfc4266bbf5749903c0ea847
David Jeske <dcjeske@tamu.edu>
2024-07-29 14:27:03 -0500

    yaml is there

--- src/main/java/com/revature/dux/DuxApplicationBackend.java
+++ src/main/java/com/revature/dux/DuxApplicationBackend.java
@@ -1,7 +1,9 @@
 package com.revature.dux;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringBootConfiguration;
 
+@SpringBootConfiguration
 public class DuxApplicationBackend {
     public static void main(String[] args) {
         SpringApplication.run(DuxApplicationBackend.class, args);
